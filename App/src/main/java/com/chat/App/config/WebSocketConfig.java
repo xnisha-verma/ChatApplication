89dafce9e7ec9c0889a9604205abacfdfccd4f7a
Nisha Verma <nisha.ve325@gmail.com>
2026-03-10 13:29:55 +0530

    updates

--- App/src/main/java/com/chat/App/config/WebSocketConfig.java
+++ App/src/main/java/com/chat/App/config/WebSocketConfig.java
@@ -13,7 +13,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOriginPatterns("*registry.addEndpoint(\"/chat\")\n" +
+                        "        .setAllowedOriginPatterns(\"*\")\n" +
+                        "        .withSockJS();")
                 .withSockJS();
     }
 
